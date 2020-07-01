1fac54f27180bdba1f41de65cf9eaf4d18048864
Cedric Patton <ceddyp117@gmail.com>
2020-07-01 03:19:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |dwarf_name, index|
-    puts index 
+    puts index +1
   end
   # Use `each_with_index` to enumerate over the provided array
   #
